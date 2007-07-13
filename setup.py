a99e985280a2637c5b33da85ffb8eb832a239514
Brian.Five <Brian.Five@d8dfe729-a134-0410-93b4-9bebaea622f3>
2007-07-13 23:04:46 +0000

    Update meta data for adding to CheeseShop

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 
-from distutils.core import setup
+from setuptools import setup
+
 setup(name='beatbox',
     version='0.9.1',
     package_dir={'': 'src'},
     packages=['beatbox'],
+    author = "Simon Fell",
+    description = "This module contains 2 versions of the Salesforce client. XMLClient which is the original beatbox version of the client which returns xmltramp objects and PythonClient? which returns dicts with proper python data types. ie integer fields return integers.",
+    license = "GPL2",
+    keywords = "python salesforce salesforce.com",
+    url = "http://code.google.com/p/salesforce-beatbox/",
     )
 
