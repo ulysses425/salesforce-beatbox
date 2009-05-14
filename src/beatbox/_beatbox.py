e5abc1998a28c495f2bfeab9331fd87bc6960040
dglick <dglick@d8dfe729-a134-0410-93b4-9bebaea622f3>
2009-05-14 05:56:12 +0000

    prep for release

--- src/beatbox/_beatbox.py
+++ src/beatbox/_beatbox.py
@@ -1,6 +1,6 @@
 """beatbox: Makes the salesforce.com SOAP API easily accessible."""
 
-__version__ = '0.12dev'
+__version__ = '0.12'
 __author__ = "Simon Fell"
 __credits__ = "Mad shouts to the sforce possie"
 __copyright__ = "(C) 2006 Simon Fell. GNU GPL 2."
