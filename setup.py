c6a5e2839e0bb112b574eeccfcc80cc32cc2188e
dglick <dglick@d8dfe729-a134-0410-93b4-9bebaea622f3>
2009-09-08 19:26:18 +0000

    fix metadata

--- setup.py
+++ setup.py
@@ -6,10 +6,11 @@ setup(name='beatbox',
     package_dir={'': 'src'},
     packages=['beatbox'],
     author = "Simon Fell et al",
+    author_email = 'plonesf@googlegroups.com',
     description = "A Python library for querying/updating Saleforce.com data via SOAP API",
     long_description = open('README.txt').read() + "\n" + open('CHANGES.txt').read(),
     license = "GNU GENERAL PUBLIC LICENSE Version 2",
     keywords = "python salesforce salesforce.com",
     url = "http://code.google.com/p/salesforce-beatbox/",
-    classifiers = ("Development Status :: 5 - Production/Stable",)
+    classifiers = ["Development Status :: 5 - Production/Stable"]
     )
