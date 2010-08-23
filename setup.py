f1abd5a5be5e939e9d8190d2b38abead152500f0
dglick <dglick@d8dfe729-a134-0410-93b4-9bebaea622f3>
2010-08-23 21:10:12 +0000

    prep for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='beatbox',
-    version='16.1', # be sure to update the version in _beatbox.py too
+    version='39.0', # be sure to update the version in _beatbox.py too
     package_dir={'': 'src'},
     packages=['beatbox'],
     author = "Simon Fell et al",
